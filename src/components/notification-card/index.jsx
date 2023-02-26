f437c8498b13bf65b0f6e9b536d4e23373eddf13
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2023-02-26 22:55:09 +0800

    init files for notif and overlay

---
+++ src/components/notification-card/index.jsx
@@ -0,0 +1,9 @@
+import './NotificationCard.css'
+
+function NotificationCard() {
+  return (
+    <h2>This is a notification</h2>
+  )
+}
+
+export default NotificationCard
---
+++ src/components/overlay/index.jsx
@@ -0,0 +1,9 @@
+import './Overlay.css'
+
+function Overlay() {
+  return (
+    <h2>This is an overlay</h2>
+  )
+}
+
+export default Overlay
