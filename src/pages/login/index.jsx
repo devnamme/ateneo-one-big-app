8242edd7a6f286b9f8d7174278a83db5ce8945e0
totallynotkai <kyla.martin@obf.ateneo.edu>
2023-02-27 13:40:13 +0800

    add desktop front-end for login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,7 +2,19 @@ import './Login.css'
 
 function LoginPage() {
   return (
-    <h1>Login Page</h1>
+    <div id="login">
+      <div id="login-container">
+        <div id="login-contents">
+            <h2>One Big App</h2>
+            <p id="OBF-email">Please login with your OBF email.</p>
+            <p id="disclaimer">One Big App only collects and stores profile information from your Google account, as well as some user-inputted information.</p>
+
+            <button id="signin">
+            </button>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
