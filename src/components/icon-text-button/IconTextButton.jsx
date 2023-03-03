9a5c8321459f596331e6b46b5285ddd272623670
Hayzhel <szel.embay@obf.ateneo.edu>
2023-03-03 10:54:30 +0800

    changed IconTextButton's props

--- src/components/icon-text-button/IconTextButton.jsx
+++ src/components/icon-text-button/IconTextButton.jsx
@@ -4,60 +4,19 @@ import './IconTextButton.css';
 const IconTextButton = (props) => {
 
     const clickHandler = () => {
-        // just placeholder for now
-        if (props.id == 'addSchedule') {
-            alert("Schedule Added");
-        }
-
-        if (props.id == 'enlistmentScheduler') {
-            alert("Enlistment Scheduled");
-        }
-        
-        if (props.id == 'addSubjectGroup') {
-            alert("Subject Group Added");
-        }
-
-        if (props.id == 'addClass') {
-            alert("Class Added");
-        }
-
-        if (props.id == 'pasteFromAISIS') {
-            alert("Pasted from AISIS");
-        }
+            alert(props.text + ' clicked.');
     }   
 
     return (
-        <button
-            id={props.id}
-            onClick = {clickHandler}
-        >   
-        <img class='icon' src={`/${props.icon}.svg`} />
-            {/* Debating whether its better to do it like this or to have a prop.text value for the text instead*/}
-            {props.id == 'addSchedule' &&
-            <p> Add New Schedule </p>
-            }
-    
-            {props.id == 'enlistmentScheduler' &&
-            <p> Enlistment Scheduler </p>
-            }
-
-            {props.id == 'addSubjectGroup' &&
-            <p> Add Subject Group </p>
-            }
-
-            {props.id == 'addClass' &&
-            <p> Add Class </p>
-            }
-
-            {props.id == 'pasteFromAISIS' &&
-            <p> Paste from AISIS </p>
-            }
+        <button onClick = {clickHandler}>   
+            <img class='icon' src={`/${props.icon}.svg`} />
+            <p> {props.text} </p>
         </button>
   );
 };
 
 IconTextButton.defaultProps = {
-    id: 'addSchedule',
+    text: 'Add Schedule',
     icon: 'add'
 }
 
