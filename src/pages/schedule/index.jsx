a4ff1f3e0c3186e7763dad51bc8641bd9cf966dc
totallynotkai <kyla.martin@obf.ateneo.edu>
2023-03-05 15:52:32 +0800

    add components for schedule, add icon text buttons to page

--- src/pages/schedule/index.jsx
+++ src/pages/schedule/index.jsx
@@ -2,18 +2,7 @@ import './Schedule.css'
 
 function SchedulePage() {
   return (
-    <div id="wrapper">
-      <div id="container">
-        <div class="content">
-          <h1>ADD NEW SCHEDULE</h1>
-          <h1>ENLISTMENT SCHEDULER</h1>
-          <h1>SCHEDULES</h1>
-        </div>
-        <div class="content">
-          <h1>TIMETABLE</h1>
-        </div>
-      </div>
-    </div>
+    <h1>Schedule Page</h1>
   )
 }
 
--- src/pages/schedules/index.jsx
+++ src/pages/schedules/index.jsx
@@ -1,8 +1,35 @@
 import './Schedules.css'
+import Header from '../../components/header/index'
+import IconTextButton from '../../components/icon-text-button/index'
+import ScheduleButton from '../../components/schedule-button/index'
+
 
 function SchedulesPage() {
   return (
-    <h1>Schedules Page</h1>
+    <div id="schedules-wrapper">
+      <div className="Header">
+          <Header/>
+      </div>
+      <div id="schedules-container">
+        <div class="schedules-content">
+           <div className="add-schedule">
+            <IconTextButton/>
+           </div>
+           <div className="enlistment-scheduler">
+            <IconTextButton text="Enlistment Scheduler" icon="schedule"/>
+           </div>
+          <div className="schedule-bar">
+            <ScheduleButton/>
+            </div>
+          <div className="schedule-bar">
+            <ScheduleButton/>
+          </div>
+        </div>
+        <div class="schedules-content">
+          <h1>TIMETABLE</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
