8109688a503a055fdb51e51cabdc3ceb16a745d0
totallynotkai <kyla.martin@obf.ateneo.edu>
2023-03-07 12:15:57 +0800

    add foundation for creator page

--- src/pages/schedule/index.jsx
+++ src/pages/schedule/index.jsx
@@ -1,8 +1,34 @@
 import './Schedule.css'
+import Header from '../../components/header/index'
+import IconTextButton from '../../components/icon-text-button/index'
+import Button from '../../components/button/button'
 
 function SchedulePage() {
   return (
-    <h1>Schedule Page</h1>
+    <div id="creator-wrapper">
+      <div className="Header">
+          <Header/>
+      </div>
+      <div id="creator-container">
+        <div class="creator-content">
+            <div className="add-class">
+              <IconTextButton text="Add Class"/>
+            </div>
+            <div className="paste-aisis">
+              <IconTextButton text="Paste from AISIS" icon="clipboard"/>
+            </div>
+            <div className="card">
+              <h1>CLASS CARD</h1>
+            </div>
+            <div className="delete-sched">
+              <Button text="Delete Schedule" isFilled={true} size="regular" color="#FF2323"/>
+            </div>
+          </div>
+        </div>
+        <div class="creator-content">
+          <h1>TIMETABLE</h1>
+        </div>
+      </div>
   )
 }
 
