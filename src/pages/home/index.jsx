216edacc4175ef49ea619f7b0aeb36c9d8bef4c4
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2023-03-07 12:26:17 +0800

    fixes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import './Home.css'
 import Header from '../../components/header/index';
 import NotificationCard from '../../components/notification-card';
+import Timetable from '../../components/timetable'
 
 function HomePage() {
   return (
     <>
       <Header />
       <NotificationCard text="Hello there!" />
+      <Timetable />
       <h1>Home Page</h1>
     </>
   )
