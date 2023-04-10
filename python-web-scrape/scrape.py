88ce25e18a767abf4aa42c646f18f144e50bdda7
Angelo <swailem.lumba@obf.ateneo.edu>
2023-04-10 20:41:47 +0800

    Formatted output and fixed scraped data

--- python-web-scrape/scrape.py
+++ python-web-scrape/scrape.py
@@ -8,8 +8,23 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 news = soup.find('div', {'class': 'row'})
-elements = news.find_all()
+elements = news.select('div.col-6.mb-4')
 
-# print the text of each element
-for element in elements:
-    print(element.text)
+# Open a file for writing
+with open('./python-web-scrape/output.txt', 'w') as f:
+    # Write the href, src, and text of each element to the file
+    for element in elements:
+        # Find the <a> tag within the element
+        link = element.find('a')
+        article = element.find('h3')
+        
+        # Get the href, src, and text attributes of the <a> tag
+        href = link.get('href').strip()
+        src = link.find('img').get('src').strip()
+        text = article.text.strip()
+        
+        f.write(f'https://www.ateneo.edu{href}\n')
+        f.write(f'https://www.ateneo.edu{src}\n')
+        f.write(f'{text}\n\n')
+
+print('Data written to output.txt')
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Scrape from '../python-web-scrape/Scrape'
 import './global.css'
 import './fonts.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Scrape />
   </React.StrictMode>
 )
