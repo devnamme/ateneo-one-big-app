c1d2b7c293b844ae70a4fb6526d20c7c28089f0c
Andre Dumandan <84612277+andredumandan@users.noreply.github.com>
2023-04-11 15:17:48 +0800

    rebased

--- src/pages/news/index.jsx
+++ src/pages/news/index.jsx
@@ -1,8 +1,52 @@
 import './News.css'
+import React, { useState, useEffect } from 'react';
 
 function NewsPage() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchTextData = async () => {
+      const response = await fetch('python-web-scrape/output.txt');
+      const data = await response.text();
+
+      const lines = data.split('\n');
+      const numItems = lines.length / 3;
+      const itemsArray = [];
+
+      for (let i = 0; i < numItems; i++) {
+        const url = lines[i * 3];
+        const photoLink = lines[i * 3 + 1];
+        const headline = lines[i * 3 + 2];
+
+        const item = {
+          url: url,
+          photoLink: photoLink,
+          headline: headline,
+        };
+
+        itemsArray.push(item);
+      }
+
+      setItems(itemsArray);
+    };
+
+    fetchTextData();
+  }, []);
+
   return (
-    <h1>News Page</h1>
+    <>
+    <h1>News</h1>
+    <div className="news-page">
+      {items.map((item, index) => (
+        <div key={index}>
+          <a href={item.url}>
+            <img src={item.photoLink} alt='photo' className='news-img' />
+            <headline>{item.headline}</headline>
+          </a>
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
